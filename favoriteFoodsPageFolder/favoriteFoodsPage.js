502f7598e77c54076d3eef0a23b98279d7f54fdd
E Alexander <erick.interiano05@gmail.com>
2021-02-26 11:02:29 -0800

    Finished work on the favorite foods javascript page

--- favoriteFoodsPageFolder/favoriteFoodsPage.js
+++ favoriteFoodsPageFolder/favoriteFoodsPage.js
@@ -8,7 +8,22 @@ for(var i = 0; i<favoriteFoods.length; i++){
     newCard.find(".nameText").text(favoriteFoods[i].name);
     newCard.find(".nameBrand").text(favoriteFoods[i].brand);
     console.log(favoriteFoods[i].name);
+    newCard.attr("listIndex", i)
 
     $('.cardHolder').append(newCard);
 }
 
+$('.cardHolder').on("click", ".card", function(){
+   var foodIndex= $(this).attr("listIndex")
+   console.log(foodIndex)
+   console.log(favoriteFoods[foodIndex])
+   $("#foodCalories").text(favoriteFoods[foodIndex].calories);
+   $("#foodFats").text(favoriteFoods[foodIndex].fats);
+   $("#foodCarbs").text(favoriteFoods[foodIndex].carbs);
+   $("#foodProteins").text(favoriteFoods[foodIndex].proteins);
+   $("#foodRecipe").text(favoriteFoods[foodIndex].recipe);
+   $("#foodIngredients").text(favoriteFoods[foodIndex].ingredients);
+
+
+
+})
